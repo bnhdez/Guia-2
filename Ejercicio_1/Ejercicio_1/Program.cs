81be76db7f2a695bd6db2d8f7fd8d787b133de36
BORIS NICHOLAS HERNANDEZ NUÑEZ <2021010562@cvirtualuees.edu.sv>
2021-07-29 15:31:40 -0600

    cambios

--- Ejercicio_1/Ejercicio_1/Program.cs
+++ Ejercicio_1/Ejercicio_1/Program.cs
@@ -6,7 +6,23 @@ namespace Ejercicio_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            int a, b;
+            Console.WriteLine("Ingrese un numero entero: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero entero: ");
+            b = int.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("El numero mayor es: " + a);
+            }
+            else if (b > a)
+            {
+                Console.WriteLine("El numero mayor es: " + b);
+            }
+            if (a == b)
+            {
+                Console.WriteLine("Los numeros son iguales");
+            }
     }
 }
