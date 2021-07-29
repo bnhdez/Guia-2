dc30d32b0cac98a5a6c41a443fa4ed67d3b1d0f1
BORIS NICHOLAS HERNANDEZ NUÑEZ <2021010562@cvirtualuees.edu.sv>
2021-07-29 16:08:39 -0600

    cambios

--- Ejercicio_2/Ejercicio_2/Program.cs
+++ Ejercicio_2/Ejercicio_2/Program.cs
@@ -18,6 +18,12 @@ namespace Ejercicio_2
                 Console.WriteLine("El numero medio es " + b);
                 Console.WriteLine("El numero pequeño es " + c);
             }
+            else if ((a > b) && (a > c) && (c >b))
+            {
+                Console.WriteLine("El numero es mayor " + a);
+                Console.WriteLine("El numero medio es " + c);
+                Console.WriteLine("El numero pequeño es " + b);
+            }
 
             if ((b > a) && (b > c) && (a > c))
             {
@@ -25,12 +31,24 @@ namespace Ejercicio_2
                 Console.WriteLine("El numero medio es " + a);
                 Console.WriteLine("El numero pequeño es " + c);
             }
+            else if ((b > a) && (b > c) && (c>a))
+            {
+                Console.WriteLine("El numero es mayor " + b);
+                Console.WriteLine("El numero medio es " + c);
+                Console.WriteLine("El numero pequeño es " + a);
+            }
 
             if ((c > a) && (c > b) && (a > b))
             {
                 Console.WriteLine("El numero es mayor " + c);
                 Console.WriteLine("El numero medio es " + a);
                 Console.WriteLine("El numero pequeño es " + b);
+            }
+            else if ((c > a) && (c > b) && (b>a))
+            {
+                Console.WriteLine("El numero es mayor " + c);
+                Console.WriteLine("El numero medio es " + b);
+                Console.WriteLine("El numero pequeño es " + a);
             }
 
         }
