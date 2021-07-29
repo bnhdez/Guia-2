1007cc95bce5df647a295a65826d9f24897eb3f5
BORIS NICHOLAS HERNANDEZ NUÑEZ <2021010562@cvirtualuees.edu.sv>
2021-07-29 16:30:11 -0600

    cammbios

---
+++ Ejercicio_3/Ejercicio_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            
+            Console.WriteLine("Ingrese los valores para 'x', 'y' y 'z'");
+            Console.WriteLine("X: ");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Y: ");
+            y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Z: ");
+            z = int.Parse(Console.ReadLine());
+
+            if ((2*x) + (20*y) - (40*z) > 585)
+            {
+                Console.WriteLine("CORRECTO");
+            }
+            else
+                {
+                Console.WriteLine("INCORRECTO");
+            }
+        }
+    }
+}
