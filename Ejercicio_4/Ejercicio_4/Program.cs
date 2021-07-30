3dacc3ae10fe907816dd871edf51d207b8e92549
BORIS NICHOLAS HERNANDEZ NUÑEZ <2021010562@cvirtualuees.edu.sv>
2021-07-29 19:32:22 -0600

    cambios

---
+++ Ejercicio_4/Ejercicio_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ejercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pago;
+            double sueldo, retencion, porcent;
+            
+            Console.WriteLine("Ingrese 'mensual' o 'quincenal' dependiendo de su sueldo");
+            pago = Console.ReadLine();
+
+            switch (pago)
+                {
+                case ("mensual"):
+                    Console.WriteLine("Es mensual");
+                    break;
+                case ("quincenal"):
+                    Console.WriteLine("Es quincenal");
+                    break;
+            }
+
+        }
+    }
+}
