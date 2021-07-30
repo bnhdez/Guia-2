cd00977bb1f4083355a45df03d96c85b58be1806
BORIS NICHOLAS HERNANDEZ NUÑEZ <2021010562@cvirtualuees.edu.sv>
2021-07-29 21:04:14 -0600

    cambios

--- Ejercicio_4/Ejercicio_4/Program.cs
+++ Ejercicio_4/Ejercicio_4/Program.cs
@@ -7,7 +7,7 @@ namespace Ejercicio_4
         static void Main(string[] args)
         {
             string pago;
-            double sueldo, retencion, porcent;
+            double sueldo, retencion, cuota, porcent;
             
             Console.WriteLine("Ingrese 'mensual' o 'quincenal' dependiendo de su sueldo");
             pago = Console.ReadLine();
@@ -15,10 +15,75 @@ namespace Ejercicio_4
             switch (pago)
                 {
                 case ("mensual"):
-                    Console.WriteLine("Es mensual");
-                    break;
+                    {
+                        Console.WriteLine("Ingrese su sueldo");
+                        sueldo = double.Parse(Console.ReadLine());
+
+                        if ((sueldo >= 0.01) && (sueldo <= 472.00))
+                        { 
+                            Console.WriteLine("SIN RETENCION");
+                        }
+                        else if ((sueldo > 472.00) && (sueldo <= 895.24))
+                        {
+                            porcent = (sueldo * 0.1);
+                            cuota = 17.67;
+                            retencion = porcent + cuota;
+
+                            Console.WriteLine("La retencion es de: " + retencion);
+                        }
+                        else if ((sueldo > 895.24) && (sueldo <= 2038.10))
+                        {
+                            porcent = (sueldo * 0.2);
+                            cuota = 60.00;
+                            retencion = porcent + cuota;
+
+                            Console.WriteLine("La retencion es de: " + retencion);
+                        }
+                        else if (sueldo > 2038.10)
+                        {
+                            porcent = (sueldo * 0.3);
+                            cuota = 288.57;
+                            retencion = porcent + cuota;
+
+                            Console.WriteLine("La retencion es de: " + retencion);
+                        }
+                        break;
+                    }
+
                 case ("quincenal"):
-                    Console.WriteLine("Es quincenal");
+
+                    Console.WriteLine("Ingrese su sueldo");
+                    sueldo = double.Parse(Console.ReadLine());
+
+                    if ((sueldo >= 0.01) && (sueldo <= 236.00))
+                    {
+                        Console.WriteLine("SIN RETENCION");
+                    }
+                    else if ((sueldo > 236.00) && (sueldo <= 447.62))
+                    {
+                        porcent = (sueldo * 0.1);
+                        cuota = 8.83;
+                        retencion = porcent + cuota;
+
+                        Console.WriteLine("La retencion es de: " + retencion);
+                    }
+                    else if ((sueldo > 447.62) && (sueldo <= 1019.05))
+                    {
+                        porcent = (sueldo * 0.2);
+                        cuota = 30.00;
+                        retencion = porcent + cuota;
+
+                        Console.WriteLine("La retencion es de: " + retencion);
+                    }
+                    else if (sueldo > 1019.06)
+                    {
+                        porcent = (sueldo * 0.3);
+                        cuota = 144.28;
+                        retencion = porcent + cuota;
+
+                        Console.WriteLine("La retencion es de: " + retencion);
+                    }
+
                     break;
             }
 
